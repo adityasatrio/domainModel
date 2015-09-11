25dd52fc494a0fb288ee49eca0b4d2417edf2b7d
Aditya <aditya.satrio.nugroho@gmail.com>
2015-09-11 14:18:49 +0700

    initial commit, contoh perbandingan anemic domain model dan rich domain model

---
+++ DomainModelPattern/src/anemic/domain/model/OrderItem.java
@@ -0,0 +1,35 @@
+package anemic.domain.model;
+
+import java.math.BigDecimal;
+
+public class OrderItem {
+
+	private BigDecimal price = BigDecimal.ZERO;
+	private int quantity;
+	private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public BigDecimal getPrice() {
+		return price;
+	}
+
+	public void setPrice(BigDecimal price) {
+		this.price = price;
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+
+}
